2a2adf94ed040d969c39c51528dde9c5c8d9a558
Lorenzo <LorenzoFrattini72@gmail.com>
2026-03-08 01:55:58 +0530

    scroll to top

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,19 +3,23 @@ import { Routes, Route } from "react-router-dom";
 import Landing from "./Pages/LandingPage/LandingPage.jsx";
 import PlacePage from "./Pages/PlacePage/PlacePage.jsx";
 import ColomboPage from "./Pages/Destinations/Colombo/ColomboPage.jsx";
+import ScrollToTop from "./Components/ScrollToTop/ScrollToTop.jsx";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Landing />} />
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Landing />} />
 
-      {/* dedicated page */}
-      <Route path="/places/colombo" element={<ColomboPage />} />
+        {/* dedicated page */}
+        <Route path="/places/colombo" element={<ColomboPage />} />
 
-      {/* all other place pages */}
-      <Route path="/places/:slug" element={<PlacePage />} />
+        {/* all other place pages */}
+        <Route path="/places/:slug" element={<PlacePage />} />
 
-      <Route path="*" element={<Landing />} />
-    </Routes>
+        <Route path="*" element={<Landing />} />
+      </Routes>
+    </>
   );
 }
